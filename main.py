4ec1e44e09d91aae65dad1d3628bd48d4afe0933
Alexander <mail-github@thekivi.tk>
2018-10-14 18:51:24 +0200

    add for loop and create var for the number

--- main.py
+++ main.py
@@ -6,138 +6,34 @@ varRotx, varRoty =600, 353
 varSchwarzx, varSchwarzy =683, 353
 varSetzenx, varSetzeny=varSchwarzx, varSchwarzy
 varZeitspanne=6
-
+varBilder="D:\\zahlen\\"
+varZahlFarbeRot = ["1","3","5","7","9","12","14","16","18","19","21","23","25","27","30","32","34","36"]
+varZahlFarbeSchwarz = ["2","4","6","8","10","11","13","15","17","20","22","24","26","28","29","31","33","35"]
 #bild = []
 #bild_io = []
-#for zahl in range(0, 37):
-#	bild.append("D:\\zahlen\%s.png" % zahl)
 #	# bild als io laden (byteio)
 
 
 try:
 	while True:
-		if pyautogui.locateCenterOnScreen('D:\\zahlen\\0.png', region=(10,400,222,280), grayscale=True):
-			varZahl="0"
-			time.sleep(varZeitspanne)	
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\1.png', region=(10,400,222,280), grayscale=True):
-			varZahl="1"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\2.png', region=(10,400,222,280), grayscale=True):
-			varZahl="2"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\3.png', region=(10,400,222,280), grayscale=True):
-			varZahl="3"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\4.png', region=(10,400,222,280), grayscale=True):
-			varZahl="4"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\5.png', region=(10,400,222,280), grayscale=True):
-			varZahl="5"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\6.png', region=(10,400,222,280), grayscale=True):
-			varZahl="6"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\7.png', region=(10,400,222,280), grayscale=True):
-			varZahl="7"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\8.png', region=(10,400,222,280), grayscale=True):
-			varZahl="8"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\9.png', region=(10,400,222,280), grayscale=True):
-			varZahl="9"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\10.png', region=(10,400,222,280), grayscale=True):
-			varZahl="10"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\11.png', region=(10,400,222,280), grayscale=True):
-			varZahl="11"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\12.png', region=(10,400,222,280), grayscale=True):
-			varZahl="12"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\13.png', region=(10,400,222,280), grayscale=True):
-			varZahl="13"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\14.png', region=(10,400,222,280), grayscale=True):
-			varZahl="14"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\15.png', region=(10,400,222,280), grayscale=True):
-			varZahl="15"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\16.png', region=(10,400,222,280), grayscale=True):
-			varZahl="16"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\17.png', region=(10,400,222,280), grayscale=True):
-			varZahl="17"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\18.png', region=(10,400,222,280), grayscale=True):
-			varZahl="18"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\19.png', region=(10,400,222,280), grayscale=True):
-			varZahl="19"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\20.png', region=(10,400,222,280), grayscale=True):
-			varZahl="20"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\21.png', region=(10,400,222,280), grayscale=True):
-			varZahl="21"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\22.png', region=(10,400,222,280), grayscale=True):
-			varZahl="22"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\23.png', region=(10,400,222,280), grayscale=True):
-			varZahl="23"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\24.png', region=(10,400,222,280), grayscale=True):
-			varZahl="24"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\25.png', region=(10,400,222,280), grayscale=True):
-			varZahl="25"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\26.png', region=(10,400,222,280), grayscale=True):
-			varZahl="26"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\27.png', region=(10,400,222,280), grayscale=True):
-			varZahl="27"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\28.png', region=(10,400,222,280), grayscale=True):
-			varZahl="28"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\29.png', region=(10,400,222,280), grayscale=True):
-			varZahl="29"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\30.png', region=(10,400,222,280), grayscale=True):
-			varZahl="30"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\31.png', region=(10,400,222,280), grayscale=True):
-			varZahl="31"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\32.png', region=(10,400,222,280), grayscale=True):
-			varZahl="32"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\33.png', region=(10,400,222,280), grayscale=True):
-			varZahl="33"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\34.png', region=(10,400,222,280), grayscale=True):
-			varZahl="34"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\35.png', region=(10,400,222,280), grayscale=True):
-			varZahl="35"
-			time.sleep(varZeitspanne)
-		elif pyautogui.locateCenterOnScreen('D:\\zahlen\\36.png', region=(10,400,222,280), grayscale=True):
-			varZahl="36"
-			time.sleep(varZeitspanne)
+		for schleifenzahl in range(0, 37):
+			if pyautogui.locateCenterOnScreen(varBilder+str(schleifenzahl)+'.png', region=(10,400,222,280), grayscale=True):
+				varZahl=str(schleifenzahl)
+				time.sleep(varZeitspanne)
+				break
+
 		if varZahl < "99":
 			print (varZahl)
-			if (varZahl == "1") or (varZahl == "3") or (varZahl == "5") or (varZahl == "7") or (varZahl == "9") or (varZahl == "12") or (varZahl == "14") or (varZahl == "16") or (varZahl == "18") or (varZahl == "19") or (varZahl == "21") or (varZahl == "23") or (varZahl == "25") or (varZahl == "27") or (varZahl == "30") or (varZahl == "32") or (varZahl == "34") or (varZahl == "36"):
+			if varZahl in varZahlFarbeRot:
 				varFarbe="rot"
 				print ("Farbe Rot")
-			else:
+			if varZahl in varZahlFarbeSchwarz:
 				varFarbe="schwarz"
 				print ("Farbe Schwarz")
 			
-#			if varZahl == "0":
-#				varFarbe="gruen"
+			if varZahl == "0":
+				varFarbe="gruen"
+				print ("Farbe gruen")
 				
 			if varFarbe == "rot":
 				varRunde=varRunde+1
@@ -155,12 +51,12 @@ try:
 				pyautogui.moveTo(915, 560)
 				time.sleep(1)
 				pyautogui.click()
-#			if varFarbe == "gruen":
-#				varRunde=varRunde+1
-#				pyautogui.click(varSetzenx, varSetzeny, clicks=varRunde)
-#				pyautogui.moveTo(915, 560)
-#				time.sleep(1)
-#				pyautogui.click()
+			if varFarbe == "gruen":
+				varRunde=varRunde+1
+				pyautogui.click(varSetzenx, varSetzeny, clicks=varRunde)
+				pyautogui.moveTo(915, 560)
+				time.sleep(1)
+				pyautogui.click()
 				
 			
 			varFarbe="nix"
